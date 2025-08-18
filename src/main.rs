5a3194b1973b8a627449067bb23fc51460d8be3b
Asciugano <samuele.diaferio@fastwebnet.it>
2025-08-18 19:35:43 +0200

    iniziato a prendere input

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ pub extern "C" fn _start() -> ! {
 
     println!("It did't crash");
 
-    loop {}
+    os::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
-    loop {}
+    os::hlt_loop();
 }
 
 #[cfg(test)]
